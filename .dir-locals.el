69acaccf6ea2290385f363ed5a8a2cbd99baa009
Kay Freyer <kay.freyer@icloud.com>
2024-11-30 19:10:36 +0100

    add some infos on installation of ocaml

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,1 +1,5 @@
-(nil . nil)
+;;; Directory Local Variables            -*- no-byte-compile: t -*-
+;;; For more information see (info "(emacs) Directory Variables")
+
+((nil . ((org-roam-directory . "/home/kay/workspace/functional-programming/functional-programming-with-ocaml/roam-files")
+         (org-roam-db-location . "/home/kay/workspace/functional-programming/functional-programming-with-ocaml/roam-files/ocaml.db"))))
